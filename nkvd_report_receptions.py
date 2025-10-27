c2210f8bfbf9e6a506fe8540be3bb8990eceb50f
vnoook <noook@yandex.ru>
2025-10-27 22:53:45 +0700

    #6 ref

--- nkvd_report_receptions.py
+++ nkvd_report_receptions.py
@@ -1,5 +1,4 @@
 # "Врач, оказавший услугу", "Услуга", "Отделение сотрудника", "Статус услуги", "Вид оплаты"
-
 import openpyxl
 
 file_xlsx = 'Аналитика2609-2710.xlsx'
@@ -22,27 +21,12 @@ for row in range(wb_file_sheet_row_begin, wb_file_sheet_row_end + 1):
     for col in range(wb_file_sheet_col_begin, wb_file_sheet_col_end + 1):
         # print(f'... {row = } ... {col = } ... {wb_s.cell(row, col).coordinate = } ... {wb_s.cell(row, col).value = }')
         pass
-
     list_all_data_fresh.append([wb_s.cell(row=row, column=2).value,
                                 wb_s.cell(row=row, column=3).value,
                                 wb_s.cell(row=row, column=10).value,
-                                # wb_s.cell(row=row, column=12).value,
                                 wb_s.cell(row=row, column=22).value
                                 ])
-
-    # if col in (2,3,10,12,22):
-    #     print(f'... {row = } ... {col = } ... {wb_s.cell(row, col).coordinate = } ... {wb_s.cell(row, col).value = }')
-
 print(*list_all_data_fresh, sep='\n')
-
-    # openpyxl.utils.cell.coordinate_from_string(wb_file_IC_s.cell(1, col_IC).coordinate)
-    # wb_full_s.cell(wb_full_s.max_row, wb_full_s.max_column).coordinate
-    # openpyxl.utils.cell.coordinate_from_string(self.wb_file_GASPS_s.cell(1, col_GASPS).coordinate)[0]
-    # wb_from_s.cell(xl_row, xl_col).coordinate
-    #
-    # indexR_IC = wb_IC_cells_range.index(row_in_range_IC)
-    # indexC_IC = row_in_range_IC.index(cell_in_row_IC)
-    # wb_IC_cell_coord = wb_IC_cells_range[indexR_IC][indexC_IC].coordinate
 
 
 
