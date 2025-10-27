c56500c59d08fa4d4ad3cf1dd43ec43d4ef5788f
vnoook <noook@yandex.ru>
2025-10-28 00:34:13 +0700

    #9 add algorithm collect data

--- nkvd_report_receptions.py
+++ nkvd_report_receptions.py
@@ -1,4 +1,4 @@
-# "Врач, оказавший услугу", "Услуга", "Отделение сотрудника", "Статус услуги", "Вид оплаты"
+# "Врач, оказавший услугу", "Услуга", "Отделение сотрудника", "Вид оплаты"
 import openpyxl
 import pandas as pd
 
@@ -17,35 +17,18 @@ wb_file_sheet_col_end = wb_s.max_column
 
 # получение всех данных из файла и его закрытие, чтобы к нему больше не возвращаться
 list_all_data_fresh = []
+otdel_dict = {}
 for row in range(wb_file_sheet_row_begin, wb_file_sheet_row_end + 1):
-    # print(row)
-    for col in range(wb_file_sheet_col_begin, wb_file_sheet_col_end + 1):
-        # print(f'... {row = } ... {col = } ... {wb_s.cell(row, col).coordinate = } ... {wb_s.cell(row, col).value = }')
-        pass
-    list_all_data_fresh.append([wb_s.cell(row=row, column=2).value,
-                                wb_s.cell(row=row, column=3).value,
-                                wb_s.cell(row=row, column=10).value,
-                                wb_s.cell(row=row, column=22).value
-                                ])
-# print(*list_all_data_fresh, sep='\n')
+    otdel = wb_s.cell(row=row, column=10).value
+    if otdel_dict.get(otdel) is None:
+        otdel_dict[otdel] = 1
+    else:
+        otdel_dict[otdel] = otdel_dict[otdel] + 1
+    doc = wb_s.cell(row=row, column=2).value
+    service = wb_s.cell(row=row, column=3).value
+    payment = wb_s.cell(row=row, column=22).value
+    list_all_data_fresh.append([doc, service, otdel, payment])
+
+print(*list_all_data_fresh, sep='\n')
 wb.close()
-# //////////////////////////////////////////////////////////////
-# Создание DataFrame на основе
-df = pd.DataFrame(list_all_data_fresh, columns=['doc', 'service', 'otdel', 'money'])
-# print(df)
 
-# ['prod_name', 'full_prod_name', 'status', 'sgtin']
-# q_prod_name = df.pivot_table('service', 'doc', aggfunc='count', fill_value = 0)
-# q_prod_name.to_excel('out1.xlsx', sheet_name='Sheet1')
-# q_prod_name = df.pivot_table('doc', 'otdel', aggfunc='count', fill_value = 0)
-# q_prod_name.to_excel('out2.xlsx', sheet_name='Sheet1')
-
-df_group1 = df.pivot_table(['otdel', 'doc', 'service', 'money'], ['otdel', 'doc'], aggfunc='count', fill_value = 0)
-df_group1.to_excel('out3.xlsx', sheet_name='Sheet1')
-
-# df_group1 = df_group1.reset_index()
-# for index, row in df_group1.iterrows():
-#     for val in ['doc', 'service', 'otdel', 'money']:
-#         print(f'{row[val] = }')
-#     print('*'*155)
-# df_group1.to_excel('out3.xlsx')
