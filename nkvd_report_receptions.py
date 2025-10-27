e5dc47ffe1c1f2aa7a4b0c76df583151e70ed4b3
vnoook <noook@yandex.ru>
2025-10-27 22:29:46 +0700

    #4 add algorithm collect data

--- nkvd_report_receptions.py
+++ nkvd_report_receptions.py
@@ -21,16 +21,28 @@ for row in range(wb_file_sheet_row_begin, wb_file_sheet_row_end + 1):
     print(row)
     for col in range(wb_file_sheet_col_begin, wb_file_sheet_col_end + 1):
         # print(f'... {row = } ... {col = } ... {wb_s.cell(row, col).value}')
-        print(f'... {row = } ... {col = } ... {openpyxl.utils.cell.coordinate_from_string(coord_string)}')
+        list_all_data_fresh.append([wb_in_s.cell(row=row, column=2).value,
+                                    wb_in_s.cell(row=row, column=3).value,
+                                    wb_in_s.cell(row=row, column=10).value,
+                                    wb_in_s.cell(row=row, column=12).value,
+                                    wb_in_s.cell(row=row, column=22).value
+                                    ])
+        # if col in (2,3,10,12,22):
+        #     print(f'... {row = } ... {col = } ... {wb_s.cell(row, col).coordinate = } ... {wb_s.cell(row, col).value = }')
 
-    # openpyxl.utils.cell.coordinate_from_string(coord_string)
-    # get_column_letter(sheet.merged_cells.ranges[0].min_col)
 
-    # list_all_data_fresh.append([wb_in_s.cell(row=row, column="Врач, оказавший услугу").value,
-    #                             wb_in_s.cell(row=row, column="Услуга").value,
-    #                             wb_in_s.cell(row=row, column="Отделение сотрудника").value,
-    #                             wb_in_s.cell(row=row, column="Статус услуги").value,
-    #                             wb_in_s.cell(row=row, column="Вид оплаты").value
-    #                             ])
+
+
+
+    # openpyxl.utils.cell.coordinate_from_string(wb_file_IC_s.cell(1, col_IC).coordinate)
+    # wb_full_s.cell(wb_full_s.max_row, wb_full_s.max_column).coordinate
+    # openpyxl.utils.cell.coordinate_from_string(self.wb_file_GASPS_s.cell(1, col_GASPS).coordinate)[0]
+    # wb_from_s.cell(xl_row, xl_col).coordinate
+    #
+    # indexR_IC = wb_IC_cells_range.index(row_in_range_IC)
+    # indexC_IC = row_in_range_IC.index(cell_in_row_IC)
+    # wb_IC_cell_coord = wb_IC_cells_range[indexR_IC][indexC_IC].coordinate
+
+
 
 # wb.close()
