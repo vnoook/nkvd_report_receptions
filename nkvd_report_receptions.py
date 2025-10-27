be21ddf27bcde422984a2a8ff73b83720dd3e115
noook <noook@yandex.ru>
2025-10-27 17:33:16 +0700

    #3

--- nkvd_report_receptions.py
+++ nkvd_report_receptions.py
@@ -5,25 +5,32 @@ import openpyxl
 file_xlsx = 'Аналитика2609-2710.xlsx'
 
 # открывается файл
-wb_in = openpyxl.load_workbook(file_xlsx)
-wb_in_s = wb_in.active
+wb = openpyxl.load_workbook(file_xlsx)
+wb_s = wb.active
 
 # строка начала и конца
-wb_in_s_row_begin = 3
-wb_in_s_row_end = wb_in_s.max_row - 1
+wb_file_sheet_row_begin = 3
+wb_file_sheet_row_end = wb_s.max_row - 1
+wb_file_sheet_col_begin = wb_s.min_column
+wb_file_sheet_col_end = wb_s.max_column
 
 list_all_data_fresh = []
 
 # получение всех данных из файла и его закрытие, чтобы к нему больше не возвращаться
-for row in range(wb_in_s_row_begin, wb_in_s_row_end + 1):
+for row in range(wb_file_sheet_row_begin, wb_file_sheet_row_end + 1):
     print(row)
-    # wb_in_s.cell(row=1, column=1).value
-    # print(f'{row = } ... {wb_in_s_row_begin = } ... {wb_in_s_row_end = }')
+    for col in range(wb_file_sheet_col_begin, wb_file_sheet_col_end + 1):
+        # print(f'... {row = } ... {col = } ... {wb_s.cell(row, col).value}')
+        print(f'... {row = } ... {col = } ... {openpyxl.utils.cell.coordinate_from_string(coord_string)}')
 
-    # list_all_data_fresh.append([wb_in_s.cell(row=row, column=wb_in_s).value,
-    #                             wb_in_s.cell(row=row, column=wb_in_s_col_2).value,
-    #                             wb_in_s.cell(row=row, column=wb_in_s_col_3).value,
-    #                             wb_in_s.cell(row=row, column=wb_in_s_col_4).value,
-    #                             wb_in_s.cell(row=row, column=wb_in_s_col_5).value
+    # openpyxl.utils.cell.coordinate_from_string(coord_string)
+    # get_column_letter(sheet.merged_cells.ranges[0].min_col)
+
+    # list_all_data_fresh.append([wb_in_s.cell(row=row, column="Врач, оказавший услугу").value,
+    #                             wb_in_s.cell(row=row, column="Услуга").value,
+    #                             wb_in_s.cell(row=row, column="Отделение сотрудника").value,
+    #                             wb_in_s.cell(row=row, column="Статус услуги").value,
+    #                             wb_in_s.cell(row=row, column="Вид оплаты").value
     #                             ])
-wb_in.close()
+
+# wb.close()
