38b1bd754b602eda94f2b6cc89c81bba9be276f6
vnoook <noook@yandex.ru>
2025-10-28 02:51:24 +0700

    #11

--- nkvd_report_receptions.py
+++ nkvd_report_receptions.py
@@ -16,44 +16,57 @@ wb_file_sheet_col_begin = wb_s.min_column
 wb_file_sheet_col_end = wb_s.max_column
 
 # получение всех данных из файла и его закрытие
-list_all_data_fresh = []
-
-otdel_dict = {}
-doc_dict = {}
-service_dict = {}
-payment_dict = {}
-
+list_all_data = []
 for row in range(wb_file_sheet_row_begin, wb_file_sheet_row_end + 1):
     otdel = wb_s.cell(row=row, column=10).value
+    doc = wb_s.cell(row=row, column=2).value
+    service = wb_s.cell(row=row, column=3).value
+    payment = wb_s.cell(row=row, column=22).value
+    list_all_data.append([otdel, doc, service, payment])
+wb.close()
+# print(*list_all_data_fresh, sep='\n')
+
+# подсчёт данных
+otdel_dict = {}
+for row in list_all_data:
+    otdel = row[0]
     if otdel_dict.get(otdel) is None:
         otdel_dict[otdel] = 1
     else:
         otdel_dict[otdel] = otdel_dict[otdel] + 1
+# print(*otdel_dict, sep='\n')
 
-    doc = wb_s.cell(row=row, column=2).value
-    if doc_dict.get(doc) is None:
-        doc_dict[doc] = 1
-    else:
-        doc_dict[doc] = doc_dict[doc] + 1
+for otdel in otdel_dict.keys():
+    for row in list_all_data:
+        if otdel == row[0]:
+            print(row[1])
+exit()
 
-    service = wb_s.cell(row=row, column=3).value
-    if service_dict.get(service) is None:
-        service_dict[service] = 1
-    else:
-        service_dict[service] = service_dict[service] + 1
+doc_dict = {}
+service_dict = {}
+payment_dict = {}
 
-    payment = wb_s.cell(row=row, column=22).value
-    if payment_dict.get(payment) is None:
-        payment_dict[payment] = 1
-    else:
-        payment_dict[payment] = payment_dict[payment] + 1
-
-    list_all_data_fresh.append([doc, service, otdel, payment])
+    # doc = row[1]
+    # service = row[2]
+    # payment = row[3]
+    #
+    # if doc_dict.get(doc) is None:
+    #     doc_dict[doc] = 1
+    # else:
+    #     doc_dict[doc] = doc_dict[doc] + 1
+    #
+    # if service_dict.get(service) is None:
+    #     service_dict[service] = 1
+    # else:
+    #     service_dict[service] = service_dict[service] + 1
+    #
+    # if payment_dict.get(payment) is None:
+    #     payment_dict[payment] = 1
+    # else:
+    #     payment_dict[payment] = payment_dict[payment] + 1
 
 print(otdel_dict)
 print(doc_dict)
 print(service_dict)
 print(payment_dict)
-# print(*list_all_data_fresh, sep='\n')
 
-wb.close()
