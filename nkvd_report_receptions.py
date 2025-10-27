6939060949be4bd662905b73f6c0aeeef19efb64
vnoook <noook@yandex.ru>
2025-10-27 22:55:24 +0700

    #7 ref

--- nkvd_report_receptions.py
+++ nkvd_report_receptions.py
@@ -1,21 +1,21 @@
 # "Врач, оказавший услугу", "Услуга", "Отделение сотрудника", "Статус услуги", "Вид оплаты"
 import openpyxl
 
+# файл данных
 file_xlsx = 'Аналитика2609-2710.xlsx'
 
 # открывается файл
 wb = openpyxl.load_workbook(file_xlsx)
 wb_s = wb.active
 
-# строка начала и конца
+# строки начала и конца
 wb_file_sheet_row_begin = 3
 wb_file_sheet_row_end = wb_s.max_row - 1
 wb_file_sheet_col_begin = wb_s.min_column
 wb_file_sheet_col_end = wb_s.max_column
 
-list_all_data_fresh = []
-
 # получение всех данных из файла и его закрытие, чтобы к нему больше не возвращаться
+list_all_data_fresh = []
 for row in range(wb_file_sheet_row_begin, wb_file_sheet_row_end + 1):
     # print(row)
     for col in range(wb_file_sheet_col_begin, wb_file_sheet_col_end + 1):
@@ -27,7 +27,4 @@ for row in range(wb_file_sheet_row_begin, wb_file_sheet_row_end + 1):
                                 wb_s.cell(row=row, column=22).value
                                 ])
 print(*list_all_data_fresh, sep='\n')
-
-
-
 wb.close()
